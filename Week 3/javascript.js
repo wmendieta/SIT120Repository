3a17cbb95741ad552f2a498872fd1e81070eed7f
wmendieta <wmendietamesa@deakin.edu.au>
2022-09-04 00:03:11 +1000

    Week 3

---
+++ Week 3/javascript.js
@@ -0,0 +1,40 @@
+console.log("Hello console Again!")
+
+// this is comment 
+
+/*
+this is multiline comment
+*/
+
+console.log("another");
+
+//console.clear();
+
+// to declare variables use (var(very old), let, const)
+
+var printMessage = "another";
+console.log(printMessage);
+
+
+let a = 2;
+let b = 4;
+sum = a + b;
+console.log(sum);
+
+const ab = 2;
+//ab = 3;
+console.log(ab);
+
+
+// Data types in Javascript
+
+let firstName = "Michael";
+console.log(typeof firstName);
+let number = 2
+let floats = 2.3
+let check = true;
+console.log(check);
+console.log(typeof floats);
+console.log(typeof check);
+let checkNull = null;
+console.log(typeof checkNull);
